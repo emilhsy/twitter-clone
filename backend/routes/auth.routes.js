51b75de54a3126c51ad720b615540f774e5a0665
emilhsy <emilhsy@gmail.com>
2025-04-08 20:46:19 +0200

    connected to mongodb and controllers

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
+import { signup, login, logout } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-    res.json({
-        data: "hello world!",
-    })
-})
+router.post("/signup", signup);
 
+router.post("/login", login);
+
+router.post("/logout", logout);
 
 export default router;
