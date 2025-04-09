255af5b242cdc4d553c9b4c14387b809c9eb4a22
emilhsy <emilhsy@gmail.com>
2025-04-09 17:56:02 +0200

    delete post functionality

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, deletePost } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", protectRoute, createPost);
 // router.post("/like/:id", protectRoute, likeUnlikePost);
 // router.post("/comment/:id", protectRoute, commentOnPost);
-// router.post("/delete", protectRoute, deletePost);
+router.post("/delete/:id", protectRoute, deletePost);
 
 export default router;
