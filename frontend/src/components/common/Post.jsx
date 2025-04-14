774cdd0a1d9236ceb6df67a304b979663144fced
emilhsy <emilhsy@gmail.com>
2025-04-14 13:04:21 +0200

    comment request from frontend

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -10,6 +10,7 @@ import { FaRegBookmark } from "react-icons/fa6";
 import { FaTrash } from "react-icons/fa";
 
 import LoadingSpinner from "./LoadingSpinner";
+import { formatPostDate } from "../../utils/date/index.js";
 
 const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
@@ -28,6 +29,14 @@ const Post = ({ post }) => {
 	});
 	
 	const queryClient = useQueryClient();
+
+	const postOwner = post.user;
+
+	const isLiked = post.likes.includes(authUser._id);
+
+	const isMyPost = authUser._id === post.user._id;
+
+	const formattedDate = formatPostDate(post.createdAt);
 
 	const { mutate: deletePost, isPending:isDeleting } = useMutation({
 		mutationFn: async () => {
@@ -81,15 +90,42 @@ const Post = ({ post }) => {
 		}
 	});
 
-	const postOwner = post.user;
-	
-	const isLiked = post.likes.includes(authUser._id);
+	const { mutate:commentPost, isPending:isCommenting } = useMutation({
+		mutationFn: async () => {
+			try {
+				const res = await fetch(`/api/posts/comment/${post._id}`, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({ text:comment })
+				});
 
-	const isMyPost = authUser._id === post.user._id;
-
-	const formattedDate = "1h";
-
-	const isCommenting = false;
+				const data = await res.json();
+				if (!res.ok) throw new Error(data.error || "Something went wrong");
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		onSuccess: (updatedPost) => {
+			toast.success("Comment posted successfully");
+			setComment("");
+			queryClient.setQueryData(["posts"], (oldData) => {
+				if (!oldData) return [];
+		
+				return oldData.map((p) => {
+					if (p._id === post._id) {
+						return { ...updatedPost };
+					}
+					return p;
+				});
+			});
+		},
+		onError: (error) => {
+			toast.error(error.message);
+		}
+	});
 
 	const handleDeletePost = () => {
 		deletePost();
@@ -97,6 +133,8 @@ const Post = ({ post }) => {
 
 	const handlePostComment = (e) => {
 		e.preventDefault();
+		if (isCommenting) return;
+		commentPost();
 	};
 
 	const handleLikePost = () => {
